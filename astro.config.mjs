fc5be6e81cac8fdfc99121c610a67c4696199b87
Linh Nguyen <linh4486@gmail.com>
2025-05-22 22:52:03 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://liinnh.github.io",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
