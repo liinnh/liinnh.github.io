5115eeceb1e5d839f40c1afa2df90464ccb5b7ac
Linh Nguyen <linh4486@gmail.com>
2025-05-26 20:10:15 -0700

    added stravas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
